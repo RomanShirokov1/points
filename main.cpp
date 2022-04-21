1c048b7b451c1e7891d9e74c982d21fb13c4e17e
RomanShirokov1 <99473055+RomanShirokov1@users.noreply.github.com>
2022-04-21 22:47:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ int main() {
 	setlocale(LC_ALL, "ru");
 
 	Point3Dh point(2, 3, 2, 3);
-	cout << "Однородные координаты точки : (2, 3, 2, 3)" << endl;
+	cout << "РћРґРЅРѕСЂРѕРґРЅС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё : (2, 3, 2, 3)" << endl;
 	point.RealCoordinates();
 
 	system("pause");
 	system("cls");
 
 	Point3Dh point1(6, 5, 5, 0);
-	cout << "Однородные координаты точки : (6, 5, 5, 0)" << endl;
+	cout << "РћРґРЅРѕСЂРѕРґРЅС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё : (6, 5, 5, 0)" << endl;
 	point1.RealCoordinates();
 
 	system("pause");
 	system("cls");
 
 	Point3Dh point2(3, 6, 1, 8);
-	cout << "Однородные координаты точки : (3, 6, 1, 8)" << endl;
+	cout << "РћРґРЅРѕСЂРѕРґРЅС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё : (3, 6, 1, 8)" << endl;
 	point2.RealCoordinates();
 
 	system("pause");
